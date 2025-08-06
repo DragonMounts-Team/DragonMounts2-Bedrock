bd969d933499ce6961efa01b1c364a5d45a668e7
tomohiko-jp <mikkurokku@yahoo.co.jp>
2025-08-06 13:38:57 +0900

    amulet

--- Dm2BP/scripts/amulet/amulet.js
+++ Dm2BP/scripts/amulet/amulet.js
@@ -10,38 +10,23 @@ const EntitiesDeny = [
 ];
 
 const AllowedMobs = [
-        "dm2:enchant_dragon",
-        "dm2:aether_dragon",
-        "dm2:ender_dragon",
-        "dm2:nether_dragon",
-        "dm2:ice_dragon",
-        "dm2:fire_dragon",
-        "dm2:forest_dragon",
-        "dm2:moonlight_dragon",
-        "dm2:sunlight_dragon",
-        "dm2:zombie_dragon",
-        "dm2:water_dragon",
-        "dm2:terra_dragon",
-         "dm2:skeleton_dragon",
-         "dm2:wither_dragon",
-        "dm2:storm_dragon",
-        "dm2:dark_dragon",
-         "dm2:f_enchant_dragon",
-        "dm2:f_aether_dragon",
-        "dm2:m_ender_dragon",
-        "dm2:f_nether_dragon",
-        "dm2:f_ice_dragon",
-        "dm2:f_fire_dragon",
-        "dm2:f_forest_dragon",
-        "dm2:f_moonlight_dragon",
-        "dm2:f_sunlight_dragon",
-        "dm2:f_water_dragon",
-        "dm2:f_terra_dragon",
-        "dm2:f_storm_dragon",
-        "dm2:f_dark_dragon",
-        "dm2:r_fire_dragon",
-        "dm2:f_phantom_dragon",
-        "dm2:phantom_dragon"
+        "dragonmountsplus:enchant_dragon",
+        "dragonmountsplus:aether_dragon",
+        "dragonmountsplus:ender_dragon",
+        "dragonmountsplus:nether_dragon",
+        "dragonmountsplus:ice_dragon",
+        "dragonmountsplus:fire_dragon",
+        "dragonmountsplus:forest_dragon",
+        "dragonmountsplus:moonlight_dragon",
+        "dragonmountsplus:sunlight_dragon",
+        "dragonmountsplus:zombie_dragon",
+        "dragonmountsplus:water_dragon",
+        "dragonmountsplus:terra_dragon",
+        "dragonmountsplus:sculk_dragon",
+        "dragonmountsplus:skeleton_dragon",
+        "dragonmountsplus:wither_dragon",
+        "dragonmountsplus:storm_dragon",
+        "dragonmountsplus:dark_dragon"
 ]  
 
 world.afterEvents.entityHitEntity.subscribe((data) => {
@@ -50,14 +35,17 @@ world.afterEvents.entityHitEntity.subscribe((data) => {
 
     const family = hitEntity.getComponent("type_family");
     if (!AllowedMobs.includes(hitEntity.typeId) || EntitiesDeny.includes(hitEntity.typeId) || hitEntity.typeId == "minecraft:painting" || family.hasTypeFamily("npc") || family.hasTypeFamily("inanimate")) return;
+    
+const isTamed = hitEntity.getComponent("minecraft:is_tamed");
+if (!isTamed || (typeof isTamed.value === "boolean" && !isTamed.value)) return;
 
     const equipment = damagingEntity.getComponent("equippable").getEquipment("Mainhand");
-    if (equipment == undefined || equipment.typeId != "dm:amulet") return;
+    if (equipment == undefined || equipment.typeId != "dragonmountsplus:amulet") return;
 
     const { x, y, z } = hitEntity.location;
     const lore = equipment.getLore();
 
-    if (equipment.getLore().length == 0 && equipment.typeId == "dm:amulet" && hitEntity.typeId != "minecraft:player") {
+    if (equipment.getLore().length == 0 && equipment.typeId == "dragonmountsplus:amulet" && hitEntity.typeId != "minecraft:player") {
         equipment.setLore([`Name: ${hitEntity.typeId}`,`ID: ${hitEntity.id}`]);
         damagingEntity.getComponent("equippable").setEquipment("Mainhand", equipment);
         hitEntity.runCommand(`ride @a[r=3.1] stop_riding`);
@@ -69,7 +57,7 @@ world.afterEvents.entityHitEntity.subscribe((data) => {
 });
 
 world.beforeEvents.worldInitialize.subscribe((data) => {
-    data.itemComponentRegistry.registerCustomComponent("dm:amulet", {
+    data.itemComponentRegistry.registerCustomComponent("dragonmountsplus:amulet", {
         onUseOn: ((event) => {
             const { block, blockFace, source, itemStack} = event;
             const pos = block.location;
